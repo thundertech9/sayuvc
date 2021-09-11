b09e54c434d2981841585561b12705ee612257b9
thundertech9 <81930771+thundertech9@users.noreply.github.com>
2021-09-11 22:29:15 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -491,7 +491,7 @@ async def play(_, message: Message):
                 
                 ],                     
                 [
-                    InlineKeyboardButton('Support', url=f't.me/RukaSupport'),
+                    InlineKeyboardButton('Support', url=f'http://t.me/RukaSupport'),
                     InlineKeyboardButton('Delete', callback_data='cls')
                 
                 ]                             
